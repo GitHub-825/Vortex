8bd8a3e2fb803a3cfde81c6a46147df3002a5878
GitHub-825 <jaygaykwad3703@gmail.com>
2024-01-17 16:04:21 +0530

    Profile Page editing

---
+++ lib/pages/profile.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 20,),
+            Center(
+              child: Container(
+                height: 150,
+                width: 150,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.deepPurpleAccent,
+                  width: 2)
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: Image(
+                      fit: BoxFit.cover,
+                      image: AssetImage("assets/images/profilepic.jpg"),
+                      loadingBuilder: (context, child, loadingProgress){
+                        if(loadingProgress == null) return child;
+                        return Center(child: CircularProgressIndicator());
+                      },
+                      errorBuilder: (context, object, stack){
+                        return Container(
+                            child: Icon(Icons.error_outline, color: Colors.red,));}
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
